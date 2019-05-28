4134ec3fc6b6bb8643db1589e4647ba882d6a63b
Jérôme <jerome.ballue@gmail.com>
2019-05-28 08:13:45 +0200

    bonjour

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Chefkrezus</title>
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="script" href="assets/js/script.js">
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+	<!-- Bootstrap core CSS -->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Material Design Bootstrap -->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.1/css/mdb.min.css" rel="stylesheet">
+</head>
+
+<!-- Just an image -->
+<nav class="navbar navbar-dark fixed-top" style="background-color: #F1F1F1">
+	<a class="navbar-brand text-center" href="#">
+		<img src="assets/img/chefkrezus.png" class="text-center" height="30" alt="mdb logo">
+	</a>
+</nav>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php  include 'header.php'?>
+<html>
+<body>
+
+
+<div class="container-fluid">
+
+
+<video class="video-fluid z-depth-1" autoplay loop controls muted>
+	<source src="assets/video/Apex-Legends.mp4" type="video/mp4" />
+</video>
+</div>
+
+<?php  include 'footer.php' ?>
+</body>
+</html>
+
